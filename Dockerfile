a2c58aaba40fd042ae72000a7aab78f402c10ce5
astapi <jyunichi.k@gmail.com>
2017-07-27 20:56:34 +0900

    add command

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM ruby:2.4.1-alpine as builder
+
+RUN apk --update add --virtual build-dependencies build-base curl-dev linux-headers git
+RUN echo 'gem: --no-document' > /etc/gemrc
+
+WORKDIR /app
+ADD . /app
+RUN bundle install --jobs=4 --without development test
+RUN apk del build-dependencies
+
+
+FROM ruby:2.4.1-alpine
+
+ENV LANG ja_JP.UTF-8
+COPY --from=builder /usr/local/bundle /usr/local/bundle
+
+ADD . /app
+RUN chown -R nobody:nogroup /app
+USER nobody
+
+WORKDIR /app
--- lib/beautiful_sa/cli.rb
+++ lib/beautiful_sa/cli.rb
@@ -1,11 +1,24 @@
 require 'beautiful_sa'
 require 'thor'
+require 'marionette'
 
 module BeautifulSa
   class CLI < Thor
-    desc "red WORD", "red words print." # コマンドの使用例と、概要
-    def red(word) # コマンドはメソッドとして定義する
-      say(word, :red)
+    desc "update_service cluster_name service_name", ""
+    def update_service(cluster_name, service_name, options = {})
+      service = Marionette::Aws::Service.new
+      current_task = service.current_task_definition(cluster_name, service_name)
+      task_name = current_task.split(':').first
+      task = Marionette::Aws::Task.new
+      task.update_task(task_name, options)
+
+      service.update_service(cluster_name, service_name, task_name)
+    end
+
+    desc "run_task cluster_name task_name", ""
+    def run_task(cluster_name, task_name)
+      task = Marionette::Aws::Task.new
+      task.run_task(cluster_name, task_name)
     end
   end
 end
